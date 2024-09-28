3eda87eacb0db6471226be97d68d56860fa905f7
abell-6103 <andrewbell4829@gmail.com>
2024-09-28 15:24:41 -0700

    changed element handling from for loop to list comprehension

--- mlb_scores.py
+++ mlb_scores.py
@@ -32,48 +32,49 @@ def ContainerIsEmpty(soup):
         return False
     return True
 
+def GetScorecardFromElement(element):
+    state_element = element.find('div',class_ = 'StatusLayerstyle__StatusContainer-sc-1s2c2o8-0 jHnypr')
+    team_name_elements = element.find_all('div',class_ = 'TeamWrappersstyle__MobileTeamWrapper-sc-uqs6qh-1 jXnGyx')
+    scoreboard_element = element.find('table',class_ = 'tablestyle__StyledTable-sc-wsl6eq-0 fxhlOg')
+
+    state_span = state_element.find('span',class_ = 'StatusLayerstyle__GameStateWrapper-sc-1s2c2o8-3 feaLYF')
+    if state_span is not None:
+        state = state_span.text
+    else:
+        state = state_element.find('div',class_='StatusLayerstyle__StatusLayerValue-sc-1s2c2o8-2 cBEKUV').contents[0].text
+
+    team_names = [x.text for x in team_name_elements]
+
+    try:
+        scoreboard_rows = scoreboard_element.find('tbody').find_all('tr')
+        scores = [int(row.find('td').text) for row in scoreboard_rows]
+    except AttributeError:
+        scores = [None,None]
+
+    scorecard = Scorecard()
+    scorecard.setState(state)
+    scorecard.setNames(team_names[0],team_names[1])
+    scorecard.setScore(scores[0],scores[1])
+
+    return scorecard
+
 def GetScores(day):
     soup = GetSoup(day)
     if soup is None:
         warnings.warn('MLB scores site did not properly load')
-        return None
+        return []
     
     gameContainer = soup.find('section',class_ = 'SnSstyle__GameCardsWrapper-sc-1m2zl7j-0 fZsilK')
     if gameContainer is None:
         warnings.warn('Score container could not be found')
-        return None
+        return []
 
     no_scores = ContainerIsEmpty(gameContainer)
     if no_scores:
-        return None
+        return []
     
     scorecard_elements = gameContainer.find_all('div',class_ = 'ScoresGamestyle__ExpandedScoresGameWrapper-sc-7t80if-0 ScoresGamestyle__DesktopScoresGameWrapper-sc-7t80if-1 gPLsYH')
-
-    scorecards = []
-    for element in scorecard_elements:
-        state_element = element.find('div',class_ = 'StatusLayerstyle__StatusContainer-sc-1s2c2o8-0 jHnypr')
-        team_name_elements = element.find_all('div',class_ = 'TeamWrappersstyle__MobileTeamWrapper-sc-uqs6qh-1 jXnGyx')
-        scoreboard_element = element.find('table',class_ = 'tablestyle__StyledTable-sc-wsl6eq-0 fxhlOg')
-
-        state_span = state_element.find('span',class_ = 'StatusLayerstyle__GameStateWrapper-sc-1s2c2o8-3 feaLYF')
-        if state_span is not None:
-            state = state_span.text
-        else:
-            state = state_element.find('div',class_='StatusLayerstyle__StatusLayerValue-sc-1s2c2o8-2 cBEKUV').contents[0].text
-
-        team_names = [x.text for x in team_name_elements]
-
-        try:
-            scoreboard_rows = scoreboard_element.find('tbody').find_all('tr')
-            scores = [int(row.find('td').text) for row in scoreboard_rows]
-        except AttributeError:
-            scores = [None,None]
-
-        scorecard = Scorecard()
-        scorecard.setState(state)
-        scorecard.setNames(team_names[0],team_names[1])
-        scorecard.setScore(scores[0],scores[1])
-        scorecards.append(scorecard)
+    scorecards = [GetScorecardFromElement(element) for element in scorecard_elements]
 
     return scorecards
 
