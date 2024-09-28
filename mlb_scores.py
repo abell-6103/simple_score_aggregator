6633458af6c6a7c085544ce0ecd2849f22f8ca50
abell-6103 <andrewbell4829@gmail.com>
2024-09-28 15:18:49 -0700

    mlb scores

--- mlb_scores.py
+++ mlb_scores.py
@@ -1,6 +1,8 @@
-from bs4 import BeautifulSoup
+from bs4 import BeautifulSoup, Tag
 import requests
 from dates import Date
+from scorecard import Scorecard
+import warnings
 
 _score_url = "https://www.mlb.com/scores/"
 
@@ -22,15 +24,65 @@ def GetSoup(day):
         return None
     return BeautifulSoup(scores_site.text,'html.parser')
 
+def ContainerIsEmpty(soup):
+    if not (isinstance(soup,BeautifulSoup) or isinstance(soup,Tag)):
+        raise TypeError('Expected BeautifulSoup or Tag object')
+    EmptyCollectionDiv = soup.find('div',class_ = 'EmptyCollectionstyle__EmptyWrapper-sc-4fquet-0 hAoHOT')
+    if EmptyCollectionDiv is None:
+        return False
+    return True
+
 def GetScores(day):
     soup = GetSoup(day)
+    if soup is None:
+        warnings.warn('MLB scores site did not properly load')
+        return None
+    
     gameContainer = soup.find('section',class_ = 'SnSstyle__GameCardsWrapper-sc-1m2zl7j-0 fZsilK')
-    print(gameContainer)
+    if gameContainer is None:
+        warnings.warn('Score container could not be found')
+        return None
+
+    no_scores = ContainerIsEmpty(gameContainer)
+    if no_scores:
+        return None
+    
+    scorecard_elements = gameContainer.find_all('div',class_ = 'ScoresGamestyle__ExpandedScoresGameWrapper-sc-7t80if-0 ScoresGamestyle__DesktopScoresGameWrapper-sc-7t80if-1 gPLsYH')
+
+    scorecards = []
+    for element in scorecard_elements:
+        state_element = element.find('div',class_ = 'StatusLayerstyle__StatusContainer-sc-1s2c2o8-0 jHnypr')
+        team_name_elements = element.find_all('div',class_ = 'TeamWrappersstyle__MobileTeamWrapper-sc-uqs6qh-1 jXnGyx')
+        scoreboard_element = element.find('table',class_ = 'tablestyle__StyledTable-sc-wsl6eq-0 fxhlOg')
+
+        state_span = state_element.find('span',class_ = 'StatusLayerstyle__GameStateWrapper-sc-1s2c2o8-3 feaLYF')
+        if state_span is not None:
+            state = state_span.text
+        else:
+            state = state_element.find('div',class_='StatusLayerstyle__StatusLayerValue-sc-1s2c2o8-2 cBEKUV').contents[0].text
+
+        team_names = [x.text for x in team_name_elements]
+
+        try:
+            scoreboard_rows = scoreboard_element.find('tbody').find_all('tr')
+            scores = [int(row.find('td').text) for row in scoreboard_rows]
+        except AttributeError:
+            scores = [None,None]
+
+        scorecard = Scorecard()
+        scorecard.setState(state)
+        scorecard.setNames(team_names[0],team_names[1])
+        scorecard.setScore(scores[0],scores[1])
+        scorecards.append(scorecard)
+
+    return scorecards
 
 def main():
     today = Date()
     today.SetToday()
-    GetScores(today)
+    scores = GetScores(today)
+    for score in scores:
+        print(score,end='\n\n')
 
 if __name__ == '__main__':
     main()
---
+++ scorecard.py
@@ -0,0 +1,26 @@
+class Scorecard:
+    def __init__(self):
+        self.name_team1 = None
+        self.name_team2 = None
+        self.score_team1 = None
+        self.score_team2 = None
+        self.game_state = None
+
+    def __repr__(self):
+        score_str = ""
+        score_str += f'{self.name_team1} @ {self.name_team2}'
+        if not (self.score_team1 is None or self.score_team2 is None):
+            score_str += f'\n{self.score_team1}-{self.score_team2}'
+        score_str += f'\n{self.game_state}'
+        return score_str
+
+    def setNames(self,name_team1,name_team2):
+        self.name_team1 = name_team1
+        self.name_team2 = name_team2
+
+    def setScore(self,score_team1,score_team2):
+        self.score_team1 = score_team1
+        self.score_team2 = score_team2
+
+    def setState(self,state):
+        self.game_state = state
